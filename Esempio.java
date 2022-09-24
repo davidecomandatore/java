603e14e38eef43ddeb6bc4e4a7d8883f562999cb
Davide Comandatore <davidecomand@hotmail.it>
2022-09-24 09:14:49 +0200

    esercizio 2 counter

---
+++ Esempio.java
@@ -0,0 +1,12 @@
+public class Esempio {
+    public static void main(String args[])
+    {   
+        int n;
+        Counter c1;
+        c1 = new Counter();
+        c1.reset();
+        c1.inc();
+        n = c1.getValue();
+        System.out.println(n); 
+    }
+}
