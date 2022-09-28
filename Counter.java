8354c2c01e7b49d322616b5b3c27c1abf619d758
Davide Comandatore <davidecomand@hotmail.it>
2022-09-28 18:43:14 +0200

    aggiornamento contatore con input

--- Counter.java
+++ Counter.java
@@ -1,15 +1,11 @@
 public class Counter {
     private int val;
-    public void reset()
-    {
-        val = 0;
-    }
-    public void inc()
-    {
-        val++;
-    }
-    public int getValue()
-    {
-        return val;
-    }
+    public  Counter() {val = 0;}
+    public  Counter(int n) {val=n;}
+    public void reset() { val = 0;  }
+    public void inc()   { val++;    }
+    public void inc(int n)   { val += n ; }
+    public void dec()   { val--; }
+    public void dec(int n)   { val -= n ; }
+    public int getValue() { return val; }
 }
--- Esempio.java
+++ Esempio.java
@@ -1,12 +1,25 @@
+import java.util.Scanner;
+
 public class Esempio {
     public static void main(String args[])
     {   
         int n;
+        int numero;
+        Scanner tastiera = new Scanner(System.in);
         Counter c1;
+
+   
+        System.out.println("digitare un numero INTERO sul quale lavorare");
+        numero = tastiera.nextInt();
         c1 = new Counter();
         c1.reset();
-        c1.inc();
+        c1.inc(numero);
+
         n = c1.getValue();
-        System.out.println(n); 
+        System.out.print("numero digitato :");
+        System.out.println(n);
+        System.out.println("viene decrementato di 2 ... ");
+        c1.dec(2);
+        System.out.println(c1.getValue());
     }
 }
